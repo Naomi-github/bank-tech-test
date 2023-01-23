1eb87b50c227ec2ad50be8871d90beba252bf2e0
Naomi Starr <naomi.starr.mail@gmail.com>
2023-01-23 13:49:25 +0000

    return 2 deposits

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -1,10 +1,20 @@
 const Account = require('../lib/account')
 
-describe('account', () => {
+describe('account deposit', () => {
   it('returns array of one deposit', () => {
-    account = new Account("deposit, 1000")
+    accountChange = new Account()
+    accountChange.addDeposit("deposit, 1000")
 
-    expect(account.deposit()).toEqual (['1000'])
+    expect(accountChange.returnDeposit()).toEqual (['1000'])
+  });
+
+  it('returns array of two deposits', () => {
+    accountChange = new Account()
+
+    accountChange.addDeposit("deposit, 1000")
+    accountChange.addDeposit("deposit, 2000")
+
+    expect(accountChange.returnDeposit()).toEqual (['2000', '1000'])
   });
 
 });
