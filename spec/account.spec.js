ca98d4f9566fa8312bbae56f0744073747f07af0
Naomi Starr <naomi.starr.mail@gmail.com>
2023-01-23 13:50:31 +0000

    account returns multiple deposits

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -17,4 +17,14 @@ describe('account deposit', () => {
     expect(accountChange.returnDeposit()).toEqual (['2000', '1000'])
   });
 
+  it('returns array of multiple deposits', () => {
+    accountChange = new Account()
+
+    accountChange.addDeposit("deposit, 1000")
+    accountChange.addDeposit("deposit, 2000")
+    accountChange.addDeposit("deposit, 500")
+
+    expect(accountChange.returnDeposit()).toEqual (['500', '2000', '1000'])
+  });
+
 });
