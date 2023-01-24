e76eba48fd32d86bd63bfb8aff37bb787fc4392d
Naomi Starr <naomi.starr.mail@gmail.com>
2023-01-24 09:55:55 +0000

    single withdrawal in accounts can be added

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -26,5 +26,15 @@ describe('account deposit', () => {
 
     expect(accountChange.returnDeposit()).toEqual (['500', '2000', '1000'])
   });
+});
+
+describe('accounts withdrawal', () => {
+  it('returns array of a single withdrawal ', () => {
+    accountChange = new Account()
+    accountChange.addWithdrawal("withdrawal, 500")
+
+    expect(accountChange.returnWithdrawal()).toEqual (['500'])
+  });
 
 });
+
