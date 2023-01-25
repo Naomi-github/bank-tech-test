063311339d2d6d82e05573d49d273a074c75490a
Naomi Starr <naomi.starr.mail@gmail.com>
2023-01-25 15:33:53 +0000

    account class refactored

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -2,80 +2,39 @@ const Account = require('../lib/account')
 
 describe('account deposits and withdraws', () => {
   it('returns a single depoist and withdrawal', () => {
-    accountChange = new Account()
-    accountChange.addDeposit("deposit, 1000")
-    accountChange.addWithdrawal("withdrawal, 500")
-    expect(accountChange.returnDeposit()).toContain('1000')
-    expect(accountChange.returnWithdrawal()).toContain('500')
+    account = new Account()
+    mockDeposits = [ '01/01/2023, 1000' ]
+    mockWithdrawals = [ '02/01/2023, 500' ]
+
+    account.addBalance(mockDeposits)
+    account.addDeposits(mockDeposits)
+    account.removeBalance(mockWithdrawals)
+    account.addWithdrawals(mockWithdrawals)
+
+    expect(account.returnAccount()).toEqual([ '01/01/2023, 1000 1000', '02/01/2023, 500 500' ])
   });
 
   it('returns multiple depoists and withdrawals', () => {
-    accountChange = new Account()
-    accountChange.addDeposit("deposit, 1000")
-    accountChange.addDeposit("deposit, 3000")
-    accountChange.addWithdrawal("withdrawal, 500")
-    accountChange.addWithdrawal("withdrawal, 900")
-    expect(accountChange.returnDeposit()).toContain('3000', '1000')
-    expect(accountChange.returnWithdrawal()).toContain('900', '500')
+    account = new Account()
+    mockDeposits = [ '01/01/2023, 1000' ]
+    mockWithdrawals = [ '02/01/2023, 500' ]
+    mockDeposits1 = [ '03/01/2023, 2000' ]
+    mockWithdrawals1 = [ '04/01/2023, 1000' ]
+
+    account.addBalance(mockDeposits)
+    account.addDeposits(mockDeposits)
+    account.removeBalance(mockWithdrawals)
+    account.addWithdrawals(mockWithdrawals)
+    account.addBalance(mockDeposits1)
+    account.addDeposits(mockDeposits1)
+    account.removeBalance(mockWithdrawals1)
+    account.addWithdrawals(mockWithdrawals1)
+
+    expect(account.returnAccount()).toEqual([
+      '01/01/2023, 1000 1000',
+      '02/01/2023, 500 500',
+      '03/01/2023, 2000 2500',
+      '04/01/2023, 1000 1500'
+    ])
   });
 });
-
-describe('account date', () => {
-  it('returns the date of a single deposit', () => {
-      accountChange = new Account()
-      accountChange.addDeposit("12-01-2023, deposit, 1000")
-      accountChange.addDate("12-01-2023, deposit, 1000")
-
-      expect(accountChange.returnDate()).toContain ('12-01-2023')
-  });
-
-  it('returns the date of multiple deposits', () => {
-      accountChange = new Account()
-      accountChange.addDeposit("12-01-2023, deposit, 1000")
-      accountChange.addDate("12-01-2023, deposit, 1000")
-      accountChange.addDeposit("12-01-2023, deposit, 1000")
-      accountChange.addDate("13-01-2023, deposit, 1000")
-
-      expect(accountChange.returnDate()).toContain ('12-01-2023')
-      expect(accountChange.returnDate()).toContain ('13-01-2023')
-  });
-
-  it('returns the date of multiple deposits and withdrawals', () => {
-      accountChange = new Account()
-      accountChange.addDeposit("12-01-2023, deposit, 1000")
-      accountChange.addDate("12-01-2023, deposit, 1000")
-      accountChange.addDeposit("13-01-2023, deposit, 1000")
-      accountChange.addDate("13-01-2023, deposit, 1000")
-      accountChange.addWithdrawal("14-01-2023, deposit, 500")
-      accountChange.addDate("14-01-2023, deposit, 500")
-      accountChange.addWithdrawal("15-01-2023, deposit, 500")
-      accountChange.addDate("15-01-2023, deposit, 500")
-
-      expect(accountChange.returnDate()).toContain ('12-01-2023')
-      expect(accountChange.returnDate()).toContain ('13-01-2023')
-
-  });
-
-  it('returns the date of multiple deposits and withdrawals', () => {
-      accountChange = new Account()
-      accountChange.addDeposit("12-01-2023, deposit, 1000")
-      accountChange.addDate("12-01-2023, deposit, 1000")
-      accountChange.addWithdrawal("14-01-2023, withdrawal, 500")
-
-      accountChange.addDate("13-01-2023, withdrawal, 1000")
-      accountChange.addDeposit("13-01-2023, deposit, 1000")
-      accountChange.addDate("14-01-2023, deposit, 500")
-      accountChange.addWithdrawal("15-01-2023, withdrawal, 500")
-      accountChange.addDate("15-01-2023, withdrawal, 500")
-
-      expect(accountChange.returnDate()).toContain ('12-01-2023')
-      expect(accountChange.returnDate()).toContain ('13-01-2023')
-
-  });
-
-});
-
-
-
-
-
